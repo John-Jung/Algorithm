1d787e1011098299d147e538e1ec30b177186db0
John.J <45717075+John-Jung@users.noreply.github.com>
2024-01-28 20:28:51 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//        int A = Integer.parseInt(br.readLine());//개행으로 받기
+//        int B = Integer.parseInt(br.readLine());
+
+//        StringTokenizer st = new StringTokenizer(br.readLine());// 한줄로 받기
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
