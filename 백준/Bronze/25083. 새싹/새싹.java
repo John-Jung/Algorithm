8c40b0d9c16d61cf5ff72374181537477122f2ad
John.J <45717075+John-Jung@users.noreply.github.com>
2024-01-31 20:52:28 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       System.out.printf("         ,r'\"7");
+       System.out.println();
+        System.out.printf("r`-_   ,'  ,/");
+        System.out.println();
+        System.out.printf(" \\. \". L_r'");
+        System.out.println();
+        System.out.printf("   `~\\/");
+        System.out.println();
+        System.out.printf("      |");
+        System.out.println();
+        System.out.printf("      |");
+        System.out.println();
+    }
+}
