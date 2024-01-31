90ac833439db4f6e4899926a03b9027d7e1682e8
John.J <45717075+John-Jung@users.noreply.github.com>
2024-02-01 01:37:57 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 444 ms, Memory: 20652 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,25 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-  
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    for (int i = 1; i <= n; i++) {
-      for (int j = 0; j < n - i; j++)
-        System.out.print(" ");
-      for (int j = 0; j < 2 * i - 1; j++)
-        System.out.print("*");
-      System.out.println();
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = N - 1; i >= 1; i--){
+            for(int j = 0; j < N - i; j++){
+                System.out.print(" ");
+            }
+            for(int j  = 0; j < 2 * i -1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
-    
-    for (int i = n - 1; i >= 1; i--) {
-      for (int j = 0; j < n - i; j++)
-        System.out.print(" ");
-      for (int j = 0; j < 2 * i - 1; j++)
-        System.out.print("*");
-      System.out.println();
-      
-    }
-  }
 }
