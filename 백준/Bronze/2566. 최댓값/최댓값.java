286093f13325d0fe6f81ed1e32e0df77a9dc9cd3
John.J <45717075+John-Jung@users.noreply.github.com>
2024-02-01 20:16:22 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 15952 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,33 +1,45 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.util.*;
+import  java.io.*;
 
-public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+public class Main{
 
-		int i,j;
-		int max = 0;
-		int x = 1, y = 1;
-		
-		int arr[][] = new int[9][9];
-		
-		for(i=0;i<9;i++) {
-			for(j=0;j<9;j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		
-		for(i=0;i<9;i++) {
-			for(j=0;j<9;j++) {
-				if(arr[i][j]>max) {
-					max = arr[i][j];
-					x = i+1;
-					y = j+1;
-				}
-			}
-		}
-		sc.close();
-		
-		System.out.println(max);
-		System.out.println(x+" "+y);
-	}
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int arr [][] = new int [9][9];
+        int max = Integer.MIN_VALUE;
+        int x = 1,y = 1;
+
+        for(int i  = 0; i < 9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(max < arr[i][j]){
+                    max = arr[i][j];
+                    x = i + 1;
+                    y = j + 1;
+                }
+
+//               System.out.println("max: " + max);
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(x + " " + y);
+
+//        for(int i  = 0; i < 9; i++){
+//            for(int j = 0; j < 9; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }System.out.println();
+//        }
+
+    }
 }
