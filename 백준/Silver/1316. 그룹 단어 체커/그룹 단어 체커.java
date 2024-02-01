0db08068ddc90d5a5975e418bd391bbbe48470bd
John.J <45717075+John-Jung@users.noreply.github.com>
2024-02-01 17:20:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import  java.io.*;
+
+public class Main{
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args)throws IOException {
+
+      int cnt = 0;
+      int N = Integer.parseInt(br.readLine());
+
+      for(int i  = 0; i < N; i++){
+          if(check() == true){
+              cnt++;
+          }
+      }
+        System.out.println(cnt);
+
+    }
+
+    public static boolean check() throws IOException{
+        boolean[] check = new boolean[26];
+        int prev = 0;
+        String  s = br.readLine();
+
+        for(int i = 0; i < s.length(); i++){
+            int now = s.charAt(i);
+
+            if(prev != now){
+
+                if(check[now - 'a'] == false){
+                    check[now- 'a'] = true;
+                    prev = now;
+                }
+
+                else {
+                    return false;
+                }
+            }
+            else {
+                continue;
+            }
+
+        }
+        return true;
+    }
+}
